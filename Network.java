43cbcf82feab3d0ef3d1ca1362208cce10870517
Sounsario <stertheblur@gmail.com>
2016-10-20 09:15:28 -0500

    Implemented non io network
    Created and init method for non input and output neurons and their
    corresponding axons.

--- Network.java
+++ Network.java
@@ -1,18 +1,17 @@
 package sterling.spencer;
 
-import java.util.ArrayList;
-
 public class Network {
 	private Neuron[] inputNeurons;
 	private Axon[][] inputAxons;
 	
 	private Neuron[] neurons;
-	private Axon[] axons;
+	private Axon[][] axons;
 	
 	private Neuron[] outputNeurons;
 	
 	public Network(double[] inputs, int numOfNeurons, int numOfOutputs) {
 		setInputs(inputs, numOfNeurons);
+		initNeuroNet(numOfNeurons);
 		initOutputs(numOfOutputs);
 	}
 	
@@ -20,15 +19,25 @@ public class Network {
 		inputNeurons = new Neuron[inputs.length];
 		inputAxons = new Axon[inputs.length][numOfNeurons];
 		
-		for(int i = 0; i < inputNeurons.length; i++) {
-			inputNeurons[i] = new Neuron(inputs[i]);
-		}
-		
 		for(int i = 0; i < inputs.length; i++) {
+			inputNeurons[i] = new Neuron(inputs[i]);
+			
 			for(int j = 0; j < numOfNeurons; j++) {
 				inputAxons[i][j] = new Axon(i,j);
 			}
-			//inputAxons[i] = new Axon();
+		}
+	}
+	
+	private void initNeuroNet(int numOfNeurons) {
+		neurons = new Neuron[numOfNeurons];
+		axons = new Axon[numOfNeurons][numOfNeurons - 1];
+		
+		for(int i = 0; i < numOfNeurons; i++) {
+			neurons[i] = new Neuron();
+			
+			for(int j = 0; j < numOfNeurons - 1; j++) {
+				axons[i][j] = new Axon(i,j);
+			}
 		}
 	}
 	
